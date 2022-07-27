c83880f12439e832e9652c199c4abd6a103e27fe
Mak <mak1312001@gmail.com>
2022-07-27 19:29:41 +0530

    update270722

--- js/custom.js
+++ js/custom.js
@@ -101,26 +101,100 @@
 
 // part 3
 
+// jQuery.noConflict();
+// jQuery(document).ready(function($){
+//     // hide image
+//     $("#btn-hide").click(function(){
+//         $("#img-id").hide(2000,function(){
+//             console.log("image is hide")
+//         });
+//     });
+
+//     // show
+//     $("#btn-show").click(function(){
+//         $("#img-id").show(5000,function(){
+//             console.log("image is show")
+//         });
+//     });
+
+//     // show/hide = toggle
+//     $("#btn-toggle").click(function(){
+//         $("#img-id").toggle(3000,function(){
+//             console.log("image is hide/show")
+//         });
+//     });
+
+//     // fadeout image
+//     $("#btn-fadeout").click(function(){
+//         $("#img-id").fadeOut();
+//     });
+
+//     // fadein image
+//     $("#btn-fadein").click(function(){
+//         $("#img-id").fadeIn(1000);
+//     });
+
+//     // fadetoggle image
+//     $("#btn-fadetoggle").click(function(){
+//         $("#img-id").fadeToggle(3000);
+//     });
+
+//     // fadetoggle image
+//     $("#btn-fadeto").click(function(){
+//         $("#img-id").fadeTo(1000,0.5);  // 1000 is speed of conversion and 0.5 is opecity
+//     });
+
+//     // slideUp image
+//     $("#btn-slideup").click(function(){
+//         $("#img-id").slideUp(2000);
+//     });
+
+//     // slideDown image
+//     $("#btn-slidedown").click(function(){
+//         $("#img-id").slideDown(2000);
+//     });
+
+//     // slidetoggle image
+//     $("#btn-slidetoggle").click(function(){
+//         $("#img-id").slideToggle(3000);
+//     });
+
+//     //  animate perform a custom animation of a set of CSS properties
+//     $("#btn-animate").click(function(){
+//         $("#zom-id").animate({left: "+=90" }, 2000, function(){
+//             console.log("animated")
+//         });
+//     });
+
+// });
+
+// part 4
+
 jQuery.noConflict();
 jQuery(document).ready(function($){
-    // hide image
-    $("#btn-hide").click(function(){
-        $("#img-id").hide(2000,function(){
-            console.log("image is hide")
-        });
-    });
+    // get text method 1
+    // let textd = $("p").text();
+    // console.log(textd);
 
-    // show
-    $("#btn-show").click(function(){
-        $("#img-id").show(5000,function(){
-            console.log("image is show")
-        });
-    });
+    // get text using button method 2
+    // $("#btn-textg").click(function(){
+    //     let te = $("h3").text();
+    //     console.log(te);
+    // });
 
-    // show/hide = toggle
-    $("#btn-toggle").click(function(){
-        $("#img-id").toggle(3000,function(){
-            console.log("image is hide/show")
-        });
+    // set Text
+    // $("#btn-texts").click(function(){
+    //     let newtext = "this is jQuery";
+    //     $("p").text(newtext);        
+    // });
+
+    // Get Html
+    let htmld = $("h3").html();
+    console.log(htmld);
+
+    // set Text
+    $("#btn-htmls").click(function(){
+        let htmltext = "<b>this is span tag</b>";
+        $("span").html(htmltext);        
     });
 });
